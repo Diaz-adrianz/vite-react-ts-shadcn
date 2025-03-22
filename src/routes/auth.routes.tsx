978d9479125ea3f2977e3f7b935504b25c135041
Diaz-Adrianz <diazadrianz191205@gmail.com>
2025-03-22 11:16:05 +0700

    style: slicing forgot passord

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,3 +1,4 @@
+import ForgotPassword from '@/pages/auth/ForgotPassword';
 import SignIn from '@/pages/auth/SignIn';
 import SignUp from '@/pages/auth/SignUp';
 import { RouteObject } from 'react-router-dom';
@@ -10,6 +11,10 @@ const authRoutes: RouteObject[] = [
   {
     path: 'sign-up',
     element: <SignUp />,
+  },
+  {
+    path: 'forgot-password',
+    element: <ForgotPassword />,
   },
 ];
 
